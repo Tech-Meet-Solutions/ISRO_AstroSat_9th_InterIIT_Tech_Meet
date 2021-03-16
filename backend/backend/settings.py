e867b1671b825372aaec18b4838077e17d6987e0
devansh-dvj <devansh.dvj@gmail.com>
2021-03-16 15:24:56 +0530

    Added app source

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'source',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
