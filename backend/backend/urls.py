f3efc5815e5e111e0f5afe856155439f3ae504e4
Poonia Purvi <pooniapurvi@cse.iitb.ac.in>
2021-03-17 02:07:41 +0530

    changed root path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import  path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('source.api.urls', 'source-api')),
+    path('', include('source.api.urls', 'source-api')),
+    #url('api/',include('source.api.urls', 'source-api'))
 ]
