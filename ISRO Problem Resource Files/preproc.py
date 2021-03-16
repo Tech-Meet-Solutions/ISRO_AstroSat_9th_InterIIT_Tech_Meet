ab6bbe4560c691d136b1efc299b695b34785915d
devansh-dvj <devansh.dvj@gmail.com>
2021-03-16 23:56:17 +0530

    Updated summary.csv

---
+++ ISRO Problem Resource Files/preproc.py
@@ -0,0 +1,29 @@
+from astropy.io import ascii
+from astropy.table import vstack, Column
+import astropy.units as u
+import numpy as np
+
+lm_tab = ascii.read("lmxbcat.dat", readme="ReadMe")
+c = Column(data=['lmxb'] * len(lm_tab), name='class', dtype='str')
+lm_tab.add_column(c)
+
+hm_tab = ascii.read("hmxbcat.dat", readme="ReadMe")
+c = Column(data=['hmxb'] * len(hm_tab), name='class', dtype='str')
+hm_tab.add_column(c)
+
+tab = vstack([lm_tab, hm_tab])
+
+ra = (tab['RAm'] + tab['RAm'] + tab['RAs']).to(u.h)
+dec = tab['DEd'].to(u.deg) + tab['DEm'].to(u.deg) + tab['DEs'].filled(0).to(u.deg)
+
+dec = np.char.add(
+    tab['DE-'].filled('+'),
+    np.array(dec).astype("str")).astype(dec.dtype) * dec.unit
+
+tab.remove_columns(['RAm', 'RAs', 'DE-', 'DEm', 'DEs'])
+tab.replace_column('RAh', ra)
+tab.replace_column('DEd', dec)
+tab['RAh'].name = 'RA'
+tab['DEd'].name = 'DE'
+
+ascii.write(tab, 'summary.csv', format='csv', overwrite=True, include_names=['RA', 'DE', 'Name', 'class'])
