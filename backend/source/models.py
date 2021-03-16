6a11481506dfd99ea165c5a4745678ba1f4e32ec
Poonia Purvi <pooniapurvi@cse.iitb.ac.in>
2021-03-17 00:02:53 +0530

    Added models

--- backend/source/models.py
+++ backend/source/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Publication(models.Model):
+    identifier = models.IntegerField(default=0)
+    Name = models.TextField()
+    URL = models.TextField()
+
+    def __str__(self):
+        return self.Name
+
+
+class Source(models.Model):
+    # Cosmic Sources
+    Name = models.TextField() # Do we need CharField?
+    RA = models.FloatField()
+    Dec = models.FloatField()
+    isObserved = models.BooleanField()
+    Publications = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.Name
+
+
