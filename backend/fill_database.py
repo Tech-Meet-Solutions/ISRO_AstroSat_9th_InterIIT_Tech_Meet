eda618a617eed64d3afe7d063d884d67a294fcea
Poonia Purvi <pooniapurvi@cse.iitb.ac.in>
2021-03-17 02:02:37 +0530

    correct script to fill db

--- backend/fill_database.py
+++ backend/fill_database.py
@@ -1,6 +1,19 @@
 import sqlite3
+import csv
+
+with open('summary.csv','r') as fin:
+    dr = csv.DictReader(fin)
+    
+    to_db = [(i['Name'], i['RA'], i['DE'], i['class']) for i in dr]
+
+
 
 con = sqlite3.connect('db.sqlite3')
 
 cursor = con.cursor()
+
+cursor.executemany("INSERT INTO source_source(Name,RA,Dec,category) VALUES(?,?,?,?)", to_db)
+
+
+con.commit()
 con.close()
