ebbeb142b031177c91c555f569ce66d691844ddb
devansh-dvj <devansh.dvj@gmail.com>
2021-03-18 07:34:22 +0530

    Fixed CORS bug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
 
     'source',
 
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'backend.urls'
