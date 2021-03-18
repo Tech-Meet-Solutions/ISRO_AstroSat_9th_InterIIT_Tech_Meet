2981a5b0a4dc6b68f7cd44a231bf1310822ee139
devansh-dvj <devansh.dvj@gmail.com>
2021-03-18 17:52:16 +0530

    Fixed makemigrations error

--- backend/fill_database.py
+++ backend/fill_database.py
@@ -2,36 +2,37 @@ import sqlite3
 import csv
 import numpy as np
 
+
 def Round(a):
-	return float(np.round(float(a), 4))
+    return float(np.round(float(a), 4))
 
 
 with open('dummy_data/publications.csv', 'r') as fin:
-	dr = csv.DictReader(fin)
-	to_db_pub = [(i['ID'], i['TITLE'], i['URL']) for i in dr]
+    dr = csv.DictReader(fin)
+    to_db_pub = [(i['ID'], i['TITLE'], i['URL']) for i in dr]
 
 with open('dummy_data/catalog_B_dummy.csv', 'r') as fin:
-	dr = csv.DictReader(fin)
-	src_pub = [( int(i['ID']), ppr ) for i in dr for ppr in i['Publications'].split(',') if ppr!='-']
+    dr = csv.DictReader(fin)
+    src_pub = [(int(i['ID']), ppr) for i in dr for ppr in i['Publications'].split(',') if ppr != '-']
 
-#To fill db with catalog B sources
+# To fill db with catalog B sources
 with open('dummy_data/catalog_B_dummy.csv', 'r') as fin:
-	dr = csv.DictReader(fin)
-	len_cat_B = 0
-	to_db_src_A = [(i['ID'], i['NAME'], Round(i['RA']), Round(i['Dec']), True, i['Desc']) for i in dr]
+    dr = csv.DictReader(fin)
+    len_cat_B = 0
+    to_db_src_A = [(i['ID'], i['NAME'], Round(i['RA']), Round(i['Dec']), True, i['Desc']) for i in dr]
 
-#count number of entries in A
+# count number of entries in A
 with open('dummy_data/catalog_B_dummy.csv', 'r') as fin:
-	dr = csv.DictReader(fin)
-	len_cat_B = 0
-	for i in dr:
-		len_cat_B += 1
+    dr = csv.DictReader(fin)
+    len_cat_B = 0
+    for i in dr:
+        len_cat_B += 1
 
-#To fill db with catalog A sources
+# To fill db with catalog A sources
 with open('dummy_data/catalog_A.csv', 'r') as fin:
     dr = csv.DictReader(fin)
-	
-    to_db_src_B = [( int(i['Id'])+len_cat_B, i['Name'], Round(i['RA']), Round(i['DE']),False, i['class']) for i in dr]
+
+    to_db_src_B = [(int(i['Id']) + len_cat_B, i['Name'], Round(i['RA']), Round(i['DE']), False, i['class']) for i in dr]
 
 
 con = sqlite3.connect('db.sqlite3')
@@ -58,13 +59,13 @@ except Exception as error:
 
 
 for entry in src_pub:
-	if entry[1][0] == "-":
-		continue
-	try:
-		cursor.execute("INSERT INTO source_source_Publications(source_id,publication_id) VALUES(?,?)", entry)
-		con.commit()
-	except Exception as error:
-		print(error)
+    if entry[1][0] == "-":
+        continue
+    try:
+        cursor.execute("INSERT INTO source_source_Publications(source_id,publication_id) VALUES(?,?)", entry)
+        con.commit()
+    except Exception as error:
+        print(error)
 
 con.commit()
 
