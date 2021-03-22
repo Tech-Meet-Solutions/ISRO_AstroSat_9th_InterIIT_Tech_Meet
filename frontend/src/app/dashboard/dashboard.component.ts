1b90302da790b2753452c56da6d4cf69b98aa817
devansh-dvj <devansh.dvj@gmail.com>
2021-03-23 04:48:56 +0530

    Added lmxb vs hmxb

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -11,14 +11,16 @@ declare var A: any;
 })
 export class DashboardComponent implements OnInit {
 
-  data: Array<Source>;
+  lmxb: Array<Source>;
+  hmxb: Array<Source>;
   aladin: any;
   plot: any;
 
   constructor(
     private server: ServerService
   ) {
-    this.data = Array<Source>();
+    this.lmxb = Array<Source>();
+    this.hmxb = Array<Source>();
     this.plot = {
       data: [],
       layout: {},
@@ -31,8 +33,12 @@ export class DashboardComponent implements OnInit {
     this.aladin.getBaseImageLayer().getColorMap().update('grayscale');
     this.server.get('/api/list/').subscribe(
       response => {
-        this.data = response.sources;
-        console.log(this.data[0]);
+        for (var i = 0; i < response.sources.length; ++i) {
+          if (response.sources[i].category === "lmxb")
+            this.lmxb.push(response.sources[i]);
+          if (response.sources[i].category == 'hmxb')
+            this.hmxb.push(response.sources[i]);
+        }
         this.update();
         this.mollweide();
         console.log("Done");
@@ -47,15 +53,36 @@ export class DashboardComponent implements OnInit {
   update(): void {
     var src_lmxb = [];
     var src_hmxb = [];
-    for (var i = 0; i < this.data.length; ++i) {
-      let si = this.data[i];
-      if (si.category === "lmxb")
-        src_lmxb.push(A.source(si.RA, si.Dec, { Name: si.Name, RA: si.RA, Dec: si.Dec, '': `<a target="_blank" href="/object/${si.id}">More Info</a>` }));
-      if (si.category == 'hmxb')
-        src_hmxb.push(A.source(si.RA, si.Dec, { Name: si.Name, RA: si.RA, Dec: si.Dec, '': `<a target="_blank" href="/object/${si.id}">More Info</a>` }));
+    for (var i = 0; i < this.lmxb.length; ++i) {
+      src_lmxb.push(
+        A.source(
+          this.lmxb[i].RA,
+          this.lmxb[i].Dec,
+          {
+            Name: this.lmxb[i].Name,
+            RA: this.lmxb[i].RA,
+            Dec: this.lmxb[i].Dec,
+            '': `<a target="_blank" href="/object/${this.lmxb[i].id}">More Info</a>`
+          }
+        )
+      );
     }
-    var cat_lmxb = A.catalog({ shape: 'square', color: '#5d5', onClick: 'showPopup', sourceSize: 16 });
-    var cat_hmxb = A.catalog({ shape: 'circle', color: '#f00', onClick: 'showPopup', sourceSize: 16 });
+    for (var i = 0; i < this.hmxb.length; ++i) {
+      src_hmxb.push(
+        A.source(
+          this.hmxb[i].RA,
+          this.hmxb[i].Dec,
+          {
+            Name: this.hmxb[i].Name,
+            RA: this.hmxb[i].RA,
+            Dec: this.hmxb[i].Dec,
+            '': `<a target="_blank" href="/object/${this.hmxb[i].id}">More Info</a>`
+          }
+        )
+      );
+    }
+    var cat_lmxb = A.catalog({ name: 'lmxb', shape: 'circle', color: '#5d5', onClick: 'showPopup', sourceSize: 16 });
+    var cat_hmxb = A.catalog({ name: 'hmxb', shape: 'circle', color: '#f00', onClick: 'showPopup', sourceSize: 16 });
     this.aladin.addCatalog(cat_lmxb);
     this.aladin.addCatalog(cat_hmxb);
     cat_lmxb.addSources(src_lmxb);
@@ -66,20 +93,47 @@ export class DashboardComponent implements OnInit {
 
   mollweide(): void {
     this.plot.data = [{
-      lon: this.data.map(function (value, index) { return -value.RA; }),
-      lat: this.data.map(function (value, index) { return value.Dec; }),
-      text: this.data.map(function (value, index) { return `(${value.RA}, ${value.Dec})` }),
+      lon: this.lmxb.map(function (value, index) { return -value.RA; }),
+      lat: this.lmxb.map(function (value, index) { return value.Dec; }),
+      text: this.lmxb.map(function (value, index) { return `(${value.RA}, ${value.Dec})` }),
       hoverinfo: 'text',
       hoverlabel: { bgcolor: '#41454c' },
+      marker: {
+        color: '#5d5',
+        opacity: 0.5,
+        size: 7,
+        line: {
+          color: 'rgb(231, 99, 250)',
+          width: 1
+        }
+      },
+      type: 'scattergeo'
+    },
+    {
+      lon: this.hmxb.map(function (value, index) { return -value.RA; }),
+      lat: this.hmxb.map(function (value, index) { return value.Dec; }),
+      text: this.hmxb.map(function (value, index) { return `(${value.RA}, ${value.Dec})` }),
+      hoverinfo: 'text',
+      hoverlabel: { bgcolor: '#41454c' },
+      marker: {
+        color: '#f00',
+        opacity: 0.5,
+        size: 7,
+        line: {
+          color: 'rgb(231, 99, 250)',
+          width: 1
+        }
+      },
       type: 'scattergeo'
     },
     {
       type: 'scattergeo',
+      hoverinfo: false,
       lat: [90, -90],
       lon: [-360, -360],
       mode: 'lines',
       line: {
-        width: 1,
+        width: 0.5,
         color: 'black'
       }
     }];
