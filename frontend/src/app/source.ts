3e939a84a291cc4207afd1537b766c4011a99e03
Poonia Purvi <pooniapurvi@cse.iitb.ac.in>
2021-03-22 15:29:27 +0530

    Added interface for SourceA, sourceB

--- frontend/src/app/source.ts
+++ frontend/src/app/source.ts
@@ -7,3 +7,28 @@ export interface Source {
   Publications: any;
   category: string;
 }
+
+export interface SourceA {
+  id: number;
+  Name: string;
+  RA: number;
+  Dec: number;
+  isObserved_uvit: boolean;
+  isObserved_laxpc: boolean;
+  isObserved_czti: boolean;
+  isObserved_sxt: boolean;
+  Publications: any;
+  category: string;
+  uvit:any;
+  sxt:any;
+  laxpc:any;
+  czti:any;
+}
+
+export interface SourceB {
+  id: number;
+  Name: string;
+  RA: number;
+  Dec: number;
+  category: string;
+}
