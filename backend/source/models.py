36b280fa2f826cf0ac4d144afa465c18bce6636d
Poonia Purvi <pooniapurvi@cse.iitb.ac.in>
2021-03-23 07:25:27 +0530

    added more fields in the Source models

--- backend/source/models.py
+++ backend/source/models.py
@@ -10,17 +10,32 @@ class Publication(models.Model):
     def __str__(self):
         return self.Name
 
+
+# reference class for the hmxb, lmxb references
+class Refs(models.Model):
+    id = models.CharField(default="", primary_key=True) # actual_id in [hl]mxb + [hl]mxb
+    bib = models.CharField(max_length=50,default="")
+    Name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=500)
+
+
+
 class SourceB(models.Model):
     # Cosmic Sources
     id = models.IntegerField(default=0, primary_key=True)
-    Name = models.CharField(max_length=100)
+    Object = models.CharField(max_length=100)
+    obsid = models.CharField(max_length=100)
     RA = models.FloatField()
     Dec = models.FloatField()
-    category = models.CharField(max_length=10, default="")
-    
+    instrument = models.CharField(max_length=50, default="")
+    date_time = models.CharField(max_length=30, default="")
+    proposal_id = models.CharField(max_length=50, default="")
+    target_id = models.CharField(max_length=200, default="")
+    observer = models.CharField(max_length=100, default="")
+    abstract = models.TextField()
 
     def __str__(self):
-        return self.Name
+        return self.Object
 
 
 
@@ -30,18 +45,43 @@ class SourceA(models.Model):
     # Cosmic Sources
     id = models.IntegerField(default=0, primary_key=True)
     Name = models.CharField(max_length=100)
+    Type = models.CharField(max_length=100)
     RA = models.FloatField()
     Dec = models.FloatField()
+    GLON = models.FloatField()
+    GLAT = models.FloatField()
+    Opt = models.CharField(max_length=100)
+    r_Opt = models.()
+    Vmag = models.FloatField()
+    B_V = models.FloatField()
+    U_B = models.FloatField()
+    E_BV = models.FloatField()
+    r_Vmag = 
+    Fx = models.FloatField()
+    Range = models.CharField(max_length=100)
+    r_Fx = 
+    Porb = models.FloatField()
+    Ppulse = models.FloatField()
+    r_Ppulse = 
+    Cat = models.CharField(max_length=100)
+    SpType = models.CharField(max_length=100)
+    Class = models.CharField(max_length=100)  # lmxb or hmxb
+
+
+
+
     isObserved_uvit = models.BooleanField(default=False)
     isObserved_sxt = models.BooleanField(default=False)
     isObserved_laxpc = models.BooleanField(default=False)
-    isObserved_czti = models.BooleanField(default=False)
-    Publications = models.ManyToManyField(Publication)
-    category = models.CharField(max_length=10, default="")
+    isObserved_czti = models.BooleanField(default=False)  
     uvit = models.ManyToManyField(SourceB,related_name = 'UV')
     sxt = models.ManyToManyField(SourceB,related_name='XT')
     laxpc = models.ManyToManyField(SourceB,related_name='PC')
     czti = models.ManyToManyField(SourceB,related_name='CZ')
 
+    Publications = models.ManyToManyField(Publication)
+    
     def __str__(self):
         return self.Name
+
+
