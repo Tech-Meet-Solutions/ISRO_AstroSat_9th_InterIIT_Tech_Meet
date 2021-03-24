7885d16e9a06fe3a8a6c5681a8f16ac570c0eb06
devansh-dvj <devansh.dvj@gmail.com>
2021-03-24 18:25:16 +0530

    Finished electron setup

--- frontend/main.js
+++ frontend/main.js
@@ -1,75 +1,46 @@
-import { app, BrowserWindow, screen } from 'electron';
-import * as path from 'path';
+const { app, BrowserWindow } = require('electron')
+const url = require("url");
+const path = require("path");
 
-let win, serve;
-const args = process.argv.slice(1);
-serve = args.some(val => val === '--serve');
-import * as url from 'url';
-
-if (serve) {
-  require('electron-reload')(__dirname, {
-  });
-}
+let mainWindow
 
 function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
-
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height
-  });
-
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    protocol: 'file:',
-    pathname: path.join(__dirname, '/index.html'),
-    slashes:  true
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  console.log(url.format({
+    pathname: path.join(__dirname, `/dist/index.html`),
+    protocol: "file:",
+    slashes: true,
+    hash: 'object/154'
   }));
-
   // Open the DevTools.
-  if (serve) {
-    win.webContents.openDevTools();
-  }
+  // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 
-try {
+app.on('ready', createWindow)
 
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
 
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
-
-} catch (e) {
-  // Catch Error
-  // throw e;
-}
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
