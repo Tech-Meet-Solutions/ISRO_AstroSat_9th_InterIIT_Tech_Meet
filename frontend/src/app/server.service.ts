d037ef3d6f7ed7875558617b12175d5f5e207dc8
devansh-dvj <devansh.dvj@gmail.com>
2021-03-27 04:33:40 +0530

    Changed backend link to heroku

--- frontend/src/app/server.service.ts
+++ frontend/src/app/server.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://127.0.0.1:8000';
+const baseUrl = 'http://astronetra.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
