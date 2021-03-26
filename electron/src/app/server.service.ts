9ed81d18223e35bc168985ad2fd3858f76949236
devansh-dvj <devansh.dvj@gmail.com>
2021-03-27 05:15:43 +0530

    Deployment updates

--- electron/src/app/server.service.ts
+++ electron/src/app/server.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://astronetra.herokuapp.com';
+const baseUrl = 'https://astronetra.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
